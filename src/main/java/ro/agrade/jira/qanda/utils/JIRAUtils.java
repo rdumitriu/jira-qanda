a071c6c07954e4924e17479bd61a2b8c1188356e
Radu Dumitriu <rdumitriu@gmail.com>
2013-06-01 01:43:05 +0300

    Works now for JIRA6

--- src/main/java/ro/agrade/jira/qanda/utils/JIRAUtils.java
+++ src/main/java/ro/agrade/jira/qanda/utils/JIRAUtils.java
@@ -8,7 +8,7 @@ import java.net.URI;
 import com.atlassian.jira.config.properties.ApplicationProperties;
 
 /**
- * The utilities. Right now, only to create the relative path to the
+ * The utilities. Right now, only to create the relative path to the JIRA
  *
  * @author Radu Dumitriu (rdumitriu@gmail.com)
  * @since 1.0
@@ -27,8 +27,8 @@ public final class JIRAUtils {
     public static String getRelativeJIRAPath(ApplicationProperties props) {
         URI uri = URI.create(props.getString("jira.baseurl"));
         String rawBase = uri.getPath(); //this returns the last path of the JIRA install
-        return ((rawBase.length() > 0 && rawBase.charAt(rawBase.length() - 1) == '/')
-                            ? rawBase.substring(0, rawBase.length() - 1)
-                            : "");
+        return ((rawBase.length() > 0)
+                    ? (rawBase.charAt(rawBase.length() - 1) == '/' ? rawBase.substring(0, rawBase.length() - 1) : rawBase)
+                    : "");
     }
 }
